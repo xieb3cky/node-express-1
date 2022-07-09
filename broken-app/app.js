54e1b2201fa4b12c62c2ef8e31471ec83af92b91
becky <xieb3cky@gmail.com>
2022-07-09 12:09:58 -0400

    part 2

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,53 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
+const app = express();
+
+const ExpressError = require("./expressError")
+
+
+app.use(express.json());
 
 app.post('/', function (req, res, next) {
     try {
-        let results = req.body.developers.map(async d => {
+
+        let developers = req.body.developers;
+        let devInfo = developers.map(async d => {
             return await axios.get(`https://api.github.com/users/${d}`);
         });
-        let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
 
-        return res.send(JSON.stringify(out));
-    } catch {
-        next(err);
+        let allDevData = devInfo.map(dev => (`{ name: ${dev.data}, bio: ${dev.data} }`));
+        return res.json(allDevData);
+
+    } catch (err) {
+        return next(err);
     }
 });
 
-app.listen(3000);
+app.get('/', function (req, res, next) {
+    async function getUser() {
+        console.log('hello')
+        let res = await axios.get('https://api.github.com/users/elie');
+        console.log(res)
+    }
+})
+
+// If no other route matches, respond with a 404
+app.use((req, res, next) => {
+    const e = new ExpressError("Page Not Found", 404);
+    next(e);
+});
+
+//global error handler 
+app.use(function (err, req, res, next) {
+
+    let status = err.status || 500;
+    let message = err.msg;
+    console.log(err)
+    return res.status(status).json({
+        error: { message, status }
+    });
+});
+
+app.listen(3000, function () {
+    console.log('App on port 3000');
+});
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,7 @@
 # Broken App Issues
+1. require installation of express & axios.
+2. error message : "TypeError: Cannot read properties of undefined (reading 'developers')"
+3. rror message : "err is not defined" - err needs to be pass through catch()
+4. app.use(express.json()); to parse incoming JSON data 
+5. res.json() to send json data
+6. added error handler 
